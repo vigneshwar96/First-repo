bf1e9eeb9eef1e6a7866383e229a6028a61b33ca
vigneshwar96 <vigneshkarthik96@gmail.com>
2025-11-16 19:49:36 -0500

    Change imageio import and update GIF saving method

--- GIF program/gif.py
+++ GIF program/gif.py
@@ -1,6 +1,6 @@
-import imageio.v3 as iio
+import imageio.v3 as iip
 filenames=['team-pic1.jpg', 'team-pic2.jpg']
 images=[]
 for filename in filenames:
-    images.append(iio.imread(filename))
-iio.imwrite('test.GIF', images, duration=500, loop=0) 
+    images.append(iip.imread(filename))
+iip.imwrite('test.GIF', images, duration=500, loop=0) 
